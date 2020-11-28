d142fa5d1d5884d08ca842b6d093a34d7064a30e
git <2605006319@qq.com>
2020-11-28 17:39:59 +0800

    zsj第三次修改

--- dzw_mobile_services/src/main/java/com/accp/biz/zsj/VehicleBiz.java
+++ dzw_mobile_services/src/main/java/com/accp/biz/zsj/VehicleBiz.java
@@ -3,11 +3,14 @@ package com.accp.biz.zsj;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.accp.dao.VehicleMapper;
 import com.accp.pojo.Client;
+import com.accp.pojo.Make;
+import com.accp.pojo.Motorcycle;
 import com.accp.pojo.Vehicle;
 
 @Service("vehicleBiz")
@@ -49,5 +52,26 @@ public class VehicleBiz {
      */
     public int deleteVehicle(@Param("vid")Integer vid) {
     	return dao.deleteVehicle(vid);
+    }
+    
+    /**
+     *	 根据品牌编号或者品牌名称查询品牌信息
+     */
+    public List<Make> queryMake(@Param("id")String id){
+    	return dao.queryMake(id);
+    }
+    
+    /**
+     * 	根据车型编号或者车型名称查询车辆信息
+     */
+    public List<Motorcycle> queryMotorcycle(@Param("makeid") Integer makeid,@Param("id")String id){
+    	return dao.queryMotorcycle(makeid,id);
+    }
+    
+    /**
+     * 	点击品牌的时候查询对应的车型
+     */
+    public List<Motorcycle> queryMotorcycle1(@Param("id")Integer id){
+    	return dao.queryMotorcycle1(id);
     }
 }
